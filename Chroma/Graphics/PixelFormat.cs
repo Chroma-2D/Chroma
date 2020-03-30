b08460ffe34811d368a4e0594f3156c81ef635f6
Ciastex <Ciastex@users.noreply.github.com>
2020-03-31 00:36:07 +0200

    Add render targets

---
+++ Chroma/Graphics/PixelFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chroma.SDL2;
+
+namespace Chroma.Graphics
+{
+    public enum PixelFormat
+    {
+        Alpha = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_ALPHA,
+        Luminance = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_LUMINANCE,
+        AlphaLuminance = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_LUMINANCE_ALPHA,
+        RG = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_RG,
+        RGB = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_RGB,
+        RGBA = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_RGBA,
+        YUV420P = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_YCbCr420P,
+        YUV442 = SDL_gpu.GPU_FormatEnum.GPU_FORMAT_YCbCr422
+    }
+}

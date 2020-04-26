138d18504ac7010cc80a3f275c2589f5392ecd0a
Ciastex <Ciastex@users.noreply.github.com>
2020-04-27 00:13:48 +0200

    Add per-chunk volume, default mixing channel count to 16.

--- Chroma/Audio/AudioClip.cs
+++ Chroma/Audio/AudioClip.cs
@@ -7,19 +7,37 @@ namespace Chroma.Audio
     public class AudioClip : DisposableResource
     {
         private AudioManager AudioManager { get; }
+
         internal IntPtr Handle { get; }
+
+        private byte _volume = 32;
+        public byte Volume
+        {
+            get => _volume;
+
+            set
+            {
+                EnsureNotDisposed();
+
+                _volume = value;
+                SDL_mixer.Mix_VolumeChunk(Handle, _volume);
+            }
+        }
 
         internal AudioClip(IntPtr handle, AudioManager audioManager)
         {
             Handle = handle;
             AudioManager = audioManager;
+
+            if (Handle != IntPtr.Zero)
+                SDL_mixer.Mix_VolumeChunk(Handle, Volume);
         }
 
         public void Play()
         {
             SDL_mixer.Mix_PlayChannel(
-                AudioManager.GetFreeChannel(), 
-                Handle, 
+                AudioManager.GetFreeChannel(),
+                Handle,
                 0
             );
         }

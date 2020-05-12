c0e4e244ba18a47cda5e599c72ee6c23c93c31b7
Ciastex <Ciastex@users.noreply.github.com>
2020-05-12 22:45:39 +0200

    add saving to file and byte arrays

---
+++ Chroma/Graphics/ImageFileFormat.cs
@@ -0,0 +1,11 @@
+﻿using Chroma.Natives.SDL;
+
+namespace Chroma.Graphics
+{
+    public enum ImageFileFormat
+    {
+        BMP = SDL_gpu.GPU_FileFormatEnum.GPU_FILE_BMP,
+        PNG = SDL_gpu.GPU_FileFormatEnum.GPU_FILE_PNG,
+        TGA = SDL_gpu.GPU_FileFormatEnum.GPU_FILE_TGA
+    }
+}

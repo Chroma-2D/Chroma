2c03168c25bd4061224cff0db7df1b59b2d6c23b
Ciastex <Ciastex@users.noreply.github.com>
2020-05-23 12:10:37 +0200

    Try a random shader from internets, fix dependency issues for nuget package, change 'engine' to 'framework'

--- Chroma.ExampleApp/ExampleGame.cs
+++ Chroma.ExampleApp/ExampleGame.cs
@@ -14,10 +14,7 @@ namespace Chroma.ExampleApp
         private TrueTypeFont _ttf;
         private Texture _bigpic;
         private PixelShader _ps;
-        private Texture _skeltal;
-        private string _text;
         private RenderTarget _tgt;
-        private Camera _cam;
 
         public ExampleGame()
         {
@@ -25,131 +22,32 @@ namespace Chroma.ExampleApp
 
             Window.GoWindowed(1024, 640);
             _tgt = new RenderTarget((ushort)Window.Properties.Width, (ushort)Window.Properties.Height);
-            _cam = new Camera();
         }
 
         protected override void LoadContent()
         {
             _ttf = Content.Load<TrueTypeFont>("Fonts/TAHOMA.TTF", 16);
             _bigpic = Content.Load<Texture>("Textures/bigpic.jpg");
-            _skeltal = Content.Load<Texture>("Textures/skeltal.png");
             _ps = Content.Load<PixelShader>("Shaders/sh.frag");
         }
 
         protected override void Update(float delta)
         {
-            Window.Properties.Title = $"FPS: {Window.FPS}";
-
-            _text = $"x: {_cam.X}\n" +
-                    $"y: {_cam.Y}\n" +
-                    $"z: {_cam.Z}\n" +
-                    $"far_z: {_cam.FarZ}\n" +
-                    $"near_z: {_cam.NearZ}\n" +
-                    $"zoom_x: {_cam.ZoomX}\n" +
-                    $"zoom_y: {_cam.ZoomY}\n" +
-                    $"rotation: {_cam.Rotation}\n" +
-                    $"center_origin: {_cam.UseCenteredOrigin}";
+            Window.Properties.Title = delta.ToString();
         }
 
         protected override void Draw(RenderContext context)
         {
+
             context.RenderTo(_tgt,
-                () =>
-                {
-                    _ps.Activate();
-                    _ps.SetUniform("tex2", _skeltal, 1);
-                    context.WithCamera(_cam,
-                        () => { context.DrawTexture(_bigpic, Vector2.Zero, Vector2.One, Vector2.Zero, 0f); });
-                });
+                () => { context.DrawTexture(_bigpic, Vector2.Zero, Vector2.One, Vector2.Zero, 0f); });
 
-            context.DrawTexture(_tgt, Vector2.Zero, Vector2.One, Vector2.Zero, 0f);
+            _ps.Activate();
+            _ps.SetUniform("CRT_CURVE_AMNTy", .25f);
+            _ps.SetUniform("CRT_CURVE_AMNTx", .25f);
+                context.DrawTexture(_tgt, Vector2.Zero, Vector2.One, Vector2.Zero, 0f);
             Shader.Deactivate();
-
-            context.DrawString(_ttf, _text, Vector2.Zero);
-        }
-
-        protected override void KeyPressed(KeyEventArgs e)
-        {
-            var modifier = 1;
-
-            if (e.Modifiers.HasFlag(KeyModifiers.LeftShift))
-                modifier = 5;
-
-            if (e.Modifiers.HasFlag(KeyModifiers.LeftControl))
-                modifier *= 2;
-
-            if (e.KeyCode == KeyCode.Backspace)
-            {
-                _cam.Z += modifier;
-            }
-            else if (e.KeyCode == KeyCode.Return)
-            {
-                _cam.Z -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.Up)
-            {
-                _cam.Y -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.Down)
-            {
-                _cam.Y += modifier;
-            }
-            else if (e.KeyCode == KeyCode.Left)
-            {
-                _cam.X -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.Right)
-            {
-                _cam.X += modifier;
-            }
-            else if (e.KeyCode == KeyCode.F5)
-            {
-                _cam.ZoomX += modifier;
-            }
-            else if (e.KeyCode == KeyCode.F6)
-            {
-                _cam.ZoomX -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.F7)
-            {
-                _cam.ZoomY += modifier;
-            }
-            else if (e.KeyCode == KeyCode.F8)
-            {
-                _cam.ZoomY -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.F1)
-            {
-                _cam.FarZ += modifier;
-            }
-            else if (e.KeyCode == KeyCode.F2)
-            {
-                _cam.FarZ -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.F3)
-            {
-                _cam.NearZ += modifier;
-            }
-            else if (e.KeyCode == KeyCode.F4)
-            {
-                _cam.NearZ -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.Q)
-            {
-                _cam.Rotation -= modifier;
-            }
-            else if (e.KeyCode == KeyCode.E)
-            {
-                _cam.Rotation += modifier;
-            }
-            else if (e.KeyCode == KeyCode.R)
-            {
-                _cam = new Camera();
-            }
-            else if (e.KeyCode == KeyCode.S)
-            {
-                _tgt.SetCamera(_cam);
-            }
+            context.DrawString(_ttf, $"{Window.FPS} FPS", Vector2.Zero);
         }
     }
 }

5a17706ed7b091cb3d73f8dd8168ed8ed710c370
vddCore <vddcore@outlook.com>
2020-05-30 01:01:16 +0200

    improve base particle emitter, allow it to be easily extensible

--- Chroma/Graphics/Particles/Particle.cs
+++ Chroma/Graphics/Particles/Particle.cs
@@ -4,14 +4,34 @@ namespace Chroma.Graphics.Particles
 {
     public class Particle
     {
-        public float Opacity { get; set; }
-        public float Rotation { get; set; }
+        public int InitialTTL { get; }
+        public float InitialRotation { get; }
+        public Color InitialColor { get; }
+        public Vector2 InitialScale { get; }
+        public Vector2 InitialOrigin { get; }
+        public Vector2 InitialPosition { get; }
+        public Vector2 InitialVelocity { get; }
 
-        public int TTL { get; set; }
-        
+        public int TTL { get; internal set; }
+
+        public float Rotation { get; set; }
+        public Color Color { get; set; }
+
         public Vector2 Scale { get; set; }
+        public Vector2 Origin { get; set; }
         public Vector2 Position { get; set; }
         public Vector2 Velocity { get; set; }
-        public Vector2 Direction { get; set; }
+
+        public Particle(int ttl, float rotation, Color color, Vector2 scale, Vector2 origin, Vector2 position,
+            Vector2 velocity)
+        {
+            TTL = InitialTTL = ttl;
+            Rotation = InitialRotation = rotation;
+            Color = InitialColor = color;
+            Scale = InitialScale = scale;
+            Origin = InitialOrigin = origin;
+            Position = InitialPosition = position;
+            Velocity = InitialVelocity = velocity;
+        }
     }
 }

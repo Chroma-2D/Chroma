75e7a113eb6c4fe557d2aa5316ee2f467b94ea45
Ciastex <Ciastex@users.noreply.github.com>
2020-06-22 17:20:41 +0200

    resolve merge conflict

--- Chroma.ExampleApp/ExampleGame.cs
+++ Chroma.ExampleApp/ExampleGame.cs
@@ -1,21 +1,12 @@
-﻿using System.Drawing;
-using System.Numerics;
+﻿using System.Numerics;
 using Chroma.Graphics;
-using Chroma.Graphics.Particles;
-using Chroma.Graphics.Particles.StateInitializers;
 using Chroma.Graphics.TextRendering;
-using Chroma.Graphics.TextRendering.Bitmap;
-using Chroma.Input;
 using Chroma.Input.EventArgs;
-using Color = Chroma.Graphics.Color;
 
 namespace Chroma.ExampleApp
 {
     public class ExampleGame : Game
     {
-        private BitmapFont _bmf;
-        private string _str;
-
         public ExampleGame()
         {
             Graphics.VSyncEnabled = false;
@@ -26,22 +17,6 @@ namespace Chroma.ExampleApp
 
         protected override void LoadContent()
         {
-            _bmf = Content.Load<BitmapFont>("BitmapFonts/vv.fnt");
-        }
-
-        protected override void Update(float delta)
-        {
-            _str = $"{Window.FPS}";
-
-            Window.Properties.Title = delta.ToString();
-        }
-
-        protected override void Draw(RenderContext context)
-        {
-            context.DrawString(_bmf, "Lorem ipsum dolor\nsit amet.", new Vector2(22), (c, i, p, g) =>
-            {
-                return new GlyphTransformData(p) {Color = Color.Red};
-            });
         }
 
         protected override void KeyPressed(KeyEventArgs e)

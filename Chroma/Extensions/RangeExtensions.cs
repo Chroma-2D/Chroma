810f99e96dd90bf9aff62950391149818efe7933
Ciastex <Ciastex@users.noreply.github.com>
2020-07-02 15:41:04 +0200

    Tweak default scene a bit.

---
+++ Chroma/Extensions/RangeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Chroma.Extensions
+{
+    public static class RangeExtensions
+    {
+        public static bool Includes(this Range range, int number)
+            => range.Start.Value <= number && range.End.Value > number;
+    }
+}

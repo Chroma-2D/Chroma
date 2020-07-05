6db6bd2c0e42045fd88d360f520e7ec6556893b6
vddCore <vddcore@outlook.com>
2020-07-05 10:45:32 +0200

    Fix the build, clean up project file, change default icon.

--- Chroma/Graphics/RenderContext.cs
+++ Chroma/Graphics/RenderContext.cs
@@ -391,10 +391,10 @@ namespace Chroma.Graphics
 
         public void DrawString(string text, Vector2 position,
             Func<char, int, Vector2, TrueTypeGlyph, GlyphTransformData> perCharTransform = null)
-            => DrawString(Game.DefaultFont, text, position, perCharTransform);
+            => DrawString(EmbeddedAssets.DefaultFont, text, position, perCharTransform);
 
         public void DrawString(string text, Vector2 position, Color color)
-            => DrawString(Game.DefaultFont, text, position, (c, i, p, g) => new GlyphTransformData(p) {Color = color});
+            => DrawString(EmbeddedAssets.DefaultFont, text, position, (c, i, p, g) => new GlyphTransformData(p) {Color = color});
 
         public void DrawString(TrueTypeFont font, string text, Vector2 position,
             Func<char, int, Vector2, TrueTypeGlyph, GlyphTransformData> perCharTransform = null)

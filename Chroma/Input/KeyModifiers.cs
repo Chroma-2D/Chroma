4c4621242a2fa14c826f9918cbf780003ad41e40
Ciastex <Ciastex@users.noreply.github.com>
2020-07-07 22:08:24 +0200

    Add wildcard key modifiers

--- Chroma/Input/KeyModifiers.cs
+++ Chroma/Input/KeyModifiers.cs
@@ -1,21 +1,32 @@
 ﻿using System;
+using Chroma.Natives.SDL;
 
 namespace Chroma.Input
 {
     [Flags]
     public enum KeyModifiers
     {
-        None = 0x0000,
-        LeftShift = 0x0001,
-        RightShift = 0x0002,
-        LeftControl = 0x0040,
-        RightControl = 0x0080,
-        LeftAlt = 0x0100,
-        RightAlt = 0x0200,
-        LeftSuper = 0x0400,
-        RightSuper = 0x0800,
-        NumLock = 0x1000,
-        CapsLock = 0x2000,
-        Mode = 0x4000
+        None = SDL2.SDL_Keymod.KMOD_NONE,
+        
+        Shift = SDL2.SDL_Keymod.KMOD_SHIFT,
+        LeftShift = SDL2.SDL_Keymod.KMOD_LSHIFT,
+        RightShift = SDL2.SDL_Keymod.KMOD_RSHIFT,
+        
+        Control = SDL2.SDL_Keymod.KMOD_CTRL,
+        LeftControl = SDL2.SDL_Keymod.KMOD_LCTRL,
+        RightControl = SDL2.SDL_Keymod.KMOD_RCTRL,
+        
+        Alt = SDL2.SDL_Keymod.KMOD_ALT,
+        LeftAlt = SDL2.SDL_Keymod.KMOD_LALT,
+        RightAlt = SDL2.SDL_Keymod.KMOD_RALT,
+        
+        Super = SDL2.SDL_Keymod.KMOD_GUI,
+        LeftSuper = SDL2.SDL_Keymod.KMOD_LGUI,
+        RightSuper = SDL2.SDL_Keymod.KMOD_RGUI,
+        
+        NumLock = SDL2.SDL_Keymod.KMOD_NUM,
+        CapsLock = SDL2.SDL_Keymod.KMOD_CAPS,
+        
+        Mode = SDL2.SDL_Keymod.KMOD_MODE
     }
 }

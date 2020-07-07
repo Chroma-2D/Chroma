894d4ff08b573f0f74d89d747a5f6f63e571fe5b
vddCore <vddcore@outlook.com>
2020-07-07 22:21:22 +0200

    Add clipboard example.

---
+++ Chroma.Examples/Clipboard/GameCore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+using Chroma;
+using Chroma.Graphics;
+using Chroma.Input;
+using Chroma.Input.EventArgs;
+
+namespace Clipboard
+{
+    public class GameCore : Game
+    {
+        protected override void Draw(RenderContext context)
+        {
+            context.DrawString(
+                "Use <F1> to set clipboard text to the current date and time.\n\n" + 
+                $"Current clipboard text: {Chroma.Clipboard.Text}\n" +
+                $"Clipboard has text? {Chroma.Clipboard.HasText}",
+                new Vector2(8)
+            );
+        }
+
+        protected override void KeyPressed(KeyEventArgs e)
+        {
+            if (e.KeyCode == KeyCode.F1)
+            {
+                Chroma.Clipboard.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ Chroma.Examples/Clipboard/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Clipboard
+{
+    internal class Program
+    {
+        internal static void Main(string[] args) => new GameCore().Run();
+    }
+}

d8c4770e22bec2a425b9b1ceaf9d69a21fb8f9b0
vddCore <vddcore@outlook.com>
2020-07-09 23:05:50 +0200

    improve reliability of content disposal

--- Chroma.Examples/EmptyProject/Program.cs
+++ Chroma.Examples/EmptyProject/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace EmptyProject
+﻿using Chroma.Graphics;
+
+namespace EmptyProject
 {
     internal class Program
     {
-        internal static void Main(string[] args) => new GameCore().Run();
+        internal static void Main(string[] args)
+        {
+            GraphicsManager.MultiSamplingPrecision = 4;
+            new GameCore().Run();
+        }
     }
 }

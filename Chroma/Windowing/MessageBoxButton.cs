0d430680ee6851447c8d9030d5d5c31e38f5b253
Ciastex <Ciastex@users.noreply.github.com>
2020-07-13 02:02:49 +0200

    Add a way to define callbacks for message box buttons.

--- Chroma/Windowing/MessageBoxButton.cs
+++ Chroma/Windowing/MessageBoxButton.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace Chroma.Windowing
 {
     public class MessageBoxButton
     {
         public int ID { get; internal set; }
         public string Text { get; internal set; }
+        public Action<int> Action { get; internal set; }
         
         internal MessageBoxButton() {}
     }

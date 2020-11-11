19d53f895f2b986c1e280d9b9c129384ceb8368a
vddCore <vddcore@outlook.com>
2020-11-11 11:43:19 +0100

    minimum openGL version 3 from now on

--- Chroma/Graphics/GlProfile.cs
+++ Chroma/Graphics/GlProfile.cs
@@ -1,7 +1,11 @@
+using Chroma.Natives.SDL;
+
 namespace Chroma.Graphics
 {
     public enum GlProfile
     {
-        
+        Core = SDL2.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_CORE,
+        Compatibility = SDL2.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_COMPATIBILITY,
+        ES = SDL2.SDL_GLprofile.SDL_GL_CONTEXT_PROFILE_ES
     }
 }

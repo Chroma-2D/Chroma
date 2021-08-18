e11fcd45122cd16a26b86be8e9c7153ab5934edd
vddCore <vddcore@outlook.com>
2021-08-18 19:07:27 +0200

    redesign the game controller example

--- Chroma.Examples/GameController/GameCore.cs
+++ Chroma.Examples/GameController/GameCore.cs
@@ -1,262 +1,70 @@
-﻿using System;
-using System.Numerics;
+﻿using System.Collections.Generic;
 using Chroma;
+using Chroma.Diagnostics;
 using Chroma.Graphics;
-using Chroma.Input;
 using Chroma.Input.GameControllers;
-using Chroma.Input.GameControllers.Drivers;
-using Chroma.Input.GameControllers.Drivers.Capabilities;
+using GameController.Views;
 
 namespace GameController
 {
     public class GameCore : Game
     {
-        private RenderTarget _tgt;
+        private List<GenericControllerView> _views;
 
-        private float _scale = 1.0f;
-        private float _rotation;
-        private Vector2 _position = new(32);
-        private Color _color = Color.White;
-        private Color _bgColor = Color.Black;
-        private ShapeMode _mode;
-
-        private Vector2?[] _touchPointPositions;
-        private float?[] _touchPointPressures;
-
-        public GameCore() : base(new(false, false))
+        public GameCore()
+            : base(new(false, false))
         {
+            Graphics.LimitFramerate = true;
+            Graphics.VerticalSyncMode = VerticalSyncMode.None;
+            
+            Window.GoWindowed(1024, 600, true);
+            RenderSettings.AutoClearColor = Color.DimGray;
+            
+            _views = new List<GenericControllerView>
+            {
+                new GenericControllerView(Window),
+                new DualShockControllerView(Window),
+                new DualSenseControllerView(Window),
+                new NintendoControllerView(Window)
+            };
+
+            // Enabled to simplify label rendering.
+            RenderSettings.ShapeBlendingEnabled = true;
         }
 
-        protected override void LoadContent()
+        protected override void ControllerDisconnected(ControllerEventArgs e)
         {
-            _tgt = new RenderTarget(32, 32);
-            _tgt.FilteringMode = TextureFilteringMode.NearestNeighbor;
-        }
-
-        protected override void Draw(RenderContext context)
-        {
-            context.Clear(_bgColor);
-            context.RenderTo(_tgt, () =>
-            {
-                context.Clear(Color.Transparent);
-                context.Rectangle(
-                    _mode,
-                    Vector2.Zero,
-                    32, 32,
-                    _color
-                );
-            });
-
-            if (_touchPointPositions != null)
-            {
-                for (var i = 0; i < _touchPointPositions.Length; i++)
-                {
-                    if (_touchPointPositions[i] != null)
-                    {
-                        context.Circle(
-                            ShapeMode.Fill,
-                            new Vector2(
-                                Window.Size.Width * _touchPointPositions[i].Value.X,
-                                Window.Size.Height * _touchPointPositions[i].Value.Y
-                            ),
-                            _touchPointPressures[i].Value * 32,
-                            Color.White
-                        );
-                    }
-                }
-            }
-
-            context.DrawTexture(_tgt, _position + _tgt.Center, Vector2.One * _scale, _tgt.Center, _rotation);
-
-            var inputs = string.Empty;
-            for (var i = 0; i < Controller.DeviceCount; i++)
-            {
-                var buttonSet = Controller.GetActiveButtons(i);
-                if (buttonSet == null)
-                    continue;
-
-                inputs += $"Controller {i}: {string.Join(", ", buttonSet)}\n";
-            }
-
-            var gyroState = string.Empty;
-            if (Controller.Get(0) is IGyroscopeEnabled gyroEnabledDriver)
-            {
-                if (gyroEnabledDriver.GyroscopeEnabled)
-                {
-                    gyroState = $"Gyroscope position: {gyroEnabledDriver.ReadGyroscopeSensor()}";
-                }
-                else
-                {
-                    gyroState = string.Empty;
-                }
-            }
-
-            var acceleroState = string.Empty;
-            if (Controller.Get(0) is IAccelerometerEnabled acceleroEnabledDriver)
-            {
-                if (acceleroEnabledDriver.AccelerometerEnabled)
-                {
-                    acceleroState = $"Accelerometer acceleration: {acceleroEnabledDriver.ReadAccelerometerSensor()}";
-                }
-                else
-                {
-                    acceleroState = string.Empty;
-                }
-            }
-
-            context.DrawString(
-                "Use left stick to control the rectangle.\n" +
-                "Use right stick to control rumble.\n" +
-                "Use left trigger to control rotation.\n" +
-                "Use right trigger to control scale.\n" +
-                "Use A/B/X/Y to control colors.\n" +
-                "Use A/B/X/Y on player 2 to control background colors.\n" +
-                "Use left/right stick buttons to toggle between stroke and fill.\n" +
-                "Use left/right bumper to control line thickness.\n\n" +
-                inputs + "\n" +
-                gyroState + "\n" +
-                acceleroState,
-                new Vector2(8)
-            );
-        }
-
-        protected override void Update(float delta)
-        {
-            _scale = 1.0f + 16f * Controller.GetAxisValueNormalized(0, ControllerAxis.RightTrigger);
-            _rotation = 360 * Controller.GetAxisValueNormalized(0, ControllerAxis.LeftTrigger);
-
-            _position.X += 120 * delta * Controller.GetAxisValueNormalized(0, ControllerAxis.LeftStickX);
-            _position.Y += 120 * delta * Controller.GetAxisValueNormalized(0, ControllerAxis.LeftStickY);
-
-            if (Controller.IsButtonDown(1, ControllerButton.A))
-                _bgColor = Color.SlateGray;
-            else if (Controller.IsButtonDown(1, ControllerButton.B))
-                _bgColor = Color.DarkCyan;
-            else if (Controller.IsButtonDown(1, ControllerButton.X))
-                _bgColor = Color.DarkGreen;
-            else if (Controller.IsButtonDown(1, ControllerButton.Y))
-                _bgColor = Color.DarkRed;
-            else
-                _bgColor = Color.Black;
-
-            for(var i = 0; i < Controller.DeviceCount; i++)
-            {
-                var rumbleSide = Controller.GetAxisValueNormalized(i, ControllerAxis.RightStickX);
-
-                var leftIntensity = 0f;
-                var rightIntensity = 0f;
-
-                if (rumbleSide < 0)
-                {
-                    leftIntensity = MathF.Abs(rumbleSide);
-                }
-                else
-                {
-                    rightIntensity = MathF.Abs(rumbleSide);
-                }
-
-                Controller.Rumble(i, (ushort)(65535 * leftIntensity), (ushort)(65535 * rightIntensity), 1000);
-            }
-
+            for (var i = 0; i < _views.Count; i++)
+                _views[i].OnDisconnected(e);
         }
 
         protected override void ControllerConnected(ControllerEventArgs e)
         {
-            e.Controller.SetDeadZone(ControllerAxis.LeftStickX, 5500);
-            e.Controller.SetDeadZone(ControllerAxis.LeftStickY, 5500);
-            e.Controller.SetDeadZone(ControllerAxis.RightStickX, 5500);
-            e.Controller.SetDeadZone(ControllerAxis.RightStickY, 5500);
-
-            Console.WriteLine(e.Controller.Info);
-
-            if (e.Controller.Is<DualShockControllerDriver>() && e.Controller.Info.PlayerIndex == 0)
-            {
-                _touchPointPositions = new Vector2?[2];
-                _touchPointPressures = new float?[2];
-            }
+            for (var i = 0; i < _views.Count; i++)
+                _views[i].OnConnected(e);
         }
 
-        protected override void ControllerButtonPressed(ControllerButtonEventArgs e)
+        protected override void ControllerAxisMoved(ControllerAxisEventArgs e)
         {
-            if (e.Button == ControllerButton.X)
-            {
-                _color = Color.DodgerBlue;
-            }
-            else if (e.Button == ControllerButton.B)
-            {
-                _color = Color.OrangeRed;
-            }
-            else if (e.Button == ControllerButton.Y)
-            {
-                _color = Color.Yellow;
-            }
-            else if (e.Button == ControllerButton.A)
-            {
-                _color = Color.LimeGreen;
-            }
-
-            if (e.Button == ControllerButton.LeftStick)
-            {
-                _mode = ShapeMode.Fill;
-            }
-            else if (e.Button == ControllerButton.RightStick)
-            {
-                _mode = ShapeMode.Stroke;
-            }
-
-            if (e.Button == ControllerButton.LeftBumper)
-            {
-                if (RenderSettings.LineThickness - 1 >= 0)
-                    RenderSettings.LineThickness -= 1;
-            }
-            else if (e.Button == ControllerButton.RightBumper)
-            {
-                RenderSettings.LineThickness += 1;
-            }
-
-            var ds4 = e.Controller.As<DualShockControllerDriver>();
-            if (ds4 != null)
-            {
-                if (e.Button == ControllerButton.DpadLeft)
-                {
-                    ds4.GyroscopeEnabled = !ds4.GyroscopeEnabled;
-                }
-                else if (e.Button == ControllerButton.DpadRight)
-                {
-                    ds4.AccelerometerEnabled = !ds4.AccelerometerEnabled;
-                }
-
-                if (ds4.GyroscopeEnabled && ds4.AccelerometerEnabled)
-                {
-                    ds4.SetLedColor(Color.Violet);
-                }
-                else if(ds4.GyroscopeEnabled)
-                {
-                    ds4.SetLedColor(Color.CornflowerBlue);
-                }
-                else if(ds4.AccelerometerEnabled)
-                {
-                    ds4.SetLedColor(Color.Red);
-                }
-            }
+            for (var i = 0; i < _views.Count; i++)
+                _views[i].OnAxisMoved(e);
         }
 
-        protected override void ControllerTouchpadMoved(ControllerTouchpadEventArgs e)
+        protected override void Update(float delta)
         {
-            _touchPointPositions[e.FingerIndex] = e.Position;
-            _touchPointPressures[e.FingerIndex] = e.Pressure;
+            Window.Title = $"Chroma Framework - {PerformanceCounter.FPS} FPS";
+            
+            for (var i = 0; i < _views.Count; i++)
+                _views[i].Update(delta);
         }
 
-        protected override void ControllerTouchpadTouched(ControllerTouchpadEventArgs e)
+        protected override void Draw(RenderContext context)
         {
-            _touchPointPositions[e.FingerIndex] = e.Position;
-            _touchPointPressures[e.FingerIndex] = e.Pressure;
-        }
-
-        protected override void ControllerTouchpadReleased(ControllerTouchpadEventArgs e)
-        {
-            _touchPointPositions[e.FingerIndex] = null;
-            _touchPointPressures[e.FingerIndex] = e.Pressure;
+            for (var i = 0; i < _views.Count; i++)
+                _views[i].Draw(context);
+            
+            context.Line(Window.Size.Width / 2, 0, Window.Size.Width / 2, Window.Size.Height, Color.DimGray);
+            context.Line(0, Window.Size.Height / 2, Window.Size.Width, Window.Size.Height / 2, Color.DimGray);
         }
     }
 }
---
+++ Chroma.Examples/GameController/Views/GenericControllerView.cs
@@ -0,0 +1,318 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Chroma.Graphics;
+using Chroma.Input.GameControllers;
+using Chroma;
+using Chroma.Graphics.TextRendering.TrueType;
+using Chroma.Windowing;
+
+namespace GameController.Views
+{
+    public class GenericControllerView
+    {
+        protected Window _window;
+        protected RenderTarget _renderTarget;
+        
+        private Dictionary<ControllerDriver, PlayerView> _controllers = new();
+
+        private Queue<ControllerDriver> _scheduledForConnection = new();
+        private Queue<ControllerDriver> _scheduledForRemoval = new();
+
+        private float _movementSpeed = 300.0f;
+
+        public virtual string ViewName => "Generic controllers";
+        
+        public virtual Color RightStickHatColor { get; } = Color.HotPink;
+
+        public virtual List<ControllerType> AcceptedControllers { get; } = new()
+        {
+            ControllerType.Unknown,
+            ControllerType.Virtual,
+            ControllerType.Xbox360,
+            ControllerType.XboxOne,
+            ControllerType.AmazonLuna,
+            ControllerType.GoogleStadia
+        };
+
+        public virtual Dictionary<ControllerButton, Color> ButtonColors { get; } = new()
+        {
+            { ControllerButton.A, Color.Lime },
+            { ControllerButton.B, Color.Red },
+            { ControllerButton.X, Color.Blue },
+            { ControllerButton.Y, Color.Yellow }
+        };
+
+        public virtual Vector2 PositionOnScreen => new(
+            _window.Size.Width - _renderTarget.Width,
+            _window.Size.Height - _renderTarget.Height
+        );
+
+        public GenericControllerView(Window window)
+        {
+            _window = window;
+            _renderTarget = new RenderTarget(window.Size / 2);
+        }
+        
+        public virtual void OnConnected(ControllerEventArgs e)
+        {
+            if (!AcceptedControllers.Contains(e.Controller.Info.Type))
+                return;
+            
+            e.Controller.SetDeadZone(ControllerAxis.LeftStickX, 4000);
+            e.Controller.SetDeadZone(ControllerAxis.LeftStickY, 4000);
+
+            _scheduledForConnection.Enqueue(e.Controller);
+        }
+
+        public virtual void OnDisconnected(ControllerEventArgs e)
+        {
+            if (!_controllers.ContainsKey(e.Controller))
+                return;
+            
+            _scheduledForRemoval.Enqueue(e.Controller);
+        }
+        
+        public virtual void OnTouchpadMoved(ControllerTouchpadEventArgs e)
+        {
+        }
+
+        public virtual void OnTouchpadTouched(ControllerTouchpadEventArgs e)
+        {
+        }
+
+        public virtual void OnTouchpadReleased(ControllerTouchpadEventArgs e)
+        {
+        }
+
+        public virtual void OnSensorStateChanged(ControllerSensorEventArgs e)
+        {
+        }
+
+        public virtual void OnAxisMoved(ControllerAxisEventArgs e)
+        {
+        }
+
+        public virtual void OnButtonPressed(ControllerButtonEventArgs e)
+        {
+        }
+
+        public virtual void OnButtonReleased(ControllerButtonEventArgs e)
+        {
+        }
+
+        public virtual void Update(float delta)
+        {
+            while (_scheduledForConnection.Any())
+            {
+                var controller = _scheduledForConnection.Dequeue();
+                
+                _controllers.Add(
+                    controller,
+                    new PlayerView(
+                        _renderTarget.Width / 2 - 16, 
+                        _renderTarget.Height / 2 - 16, 
+                        32, 
+                        32
+                    )
+                );
+            }
+
+            foreach (var kvp in _controllers)
+            {
+                var playerView = kvp.Value;
+                
+                playerView.Rectangle.X += kvp.Key.GetAxisValueNormalized(ControllerAxis.LeftStickX) * delta * _movementSpeed;
+                playerView.Rectangle.Y += kvp.Key.GetAxisValueNormalized(ControllerAxis.LeftStickY) * delta * _movementSpeed;
+
+                if (playerView.Rectangle.X < 0)
+                    playerView.Rectangle.X = 0;
+                else if (playerView.Rectangle.X + playerView.Rectangle.Width > _renderTarget.Width)
+                    playerView.Rectangle.X = _renderTarget.Width - playerView.Rectangle.Width;
+
+                if (playerView.Rectangle.Y < 0)
+                    playerView.Rectangle.Y = 0;
+                else if (playerView.Rectangle.Y + playerView.Rectangle.Height > _renderTarget.Height)
+                    playerView.Rectangle.Y = _renderTarget.Height - playerView.Rectangle.Height;
+                
+                playerView.TriggerLeft = kvp.Key.GetAxisValueNormalized(ControllerAxis.LeftTrigger);
+                playerView.TriggerRight = kvp.Key.GetAxisValueNormalized(ControllerAxis.RightTrigger);
+                playerView.RightStickX = kvp.Key.GetAxisValueNormalized(ControllerAxis.RightStickX);
+                playerView.RightStickY = kvp.Key.GetAxisValueNormalized(ControllerAxis.RightStickY);
+                
+                _controllers[kvp.Key] = playerView;
+                
+                if (kvp.Key.IsButtonDown(ControllerButton.A))
+                    playerView.Color = ButtonColors[ControllerButton.A];
+                
+                if (kvp.Key.IsButtonDown(ControllerButton.B))
+                    playerView.Color = ButtonColors[ControllerButton.B];
+                
+                if (kvp.Key.IsButtonDown(ControllerButton.X))
+                    playerView.Color = ButtonColors[ControllerButton.X];
+                
+                if (kvp.Key.IsButtonDown(ControllerButton.Y))
+                    playerView.Color = ButtonColors[ControllerButton.Y];
+            }
+
+            while (_scheduledForRemoval.Any())
+            {
+                var controller = _scheduledForRemoval.Dequeue();
+                _controllers.Remove(controller);
+            }
+        }
+
+        public virtual void Draw(RenderContext context)
+        {
+            context.RenderTo(_renderTarget, () =>
+            {
+                context.Clear(Color.Black);
+                
+                foreach (var kvp in _controllers)
+                {
+                    var controller = kvp.Key;
+                    var player = kvp.Value;
+                    context.Rectangle(ShapeMode.Fill, player.Rectangle, player.Color);
+                    
+                    var playerLabel = controller.Info.PlayerIndex.ToString();
+                    var labelMeasure = TrueTypeFont.Default.Measure(playerLabel);
+
+                    context.DrawString(
+                        playerLabel,
+                        new Vector2(
+                            player.Rectangle.X + (player.Rectangle.Width / 2f - labelMeasure.Width / 2f),
+                            player.Rectangle.Y + (player.Rectangle.Height / 2f - labelMeasure.Height / 2f)
+                        ),
+                        Color.Black
+                    );
+
+                    var playerButtonsLabel = string.Join(", ", controller.ActiveButtons);
+                    var playerButtonsMeasure = TrueTypeFont.Default.Measure(playerButtonsLabel);
+                    
+                    context.DrawString(
+                        playerButtonsLabel,
+                        new Vector2(
+                            player.Rectangle.X + (player.Rectangle.Width / 2f - playerButtonsMeasure.Width / 2f),
+                            player.Rectangle.Y - TrueTypeFont.Default.Height - 8
+                        )
+                    );
+
+                    if (player.TriggerLeft > 0)
+                    {
+                        var triggerLabel = player.TriggerLeft.ToString("F3");
+                        var triggerMeasure = TrueTypeFont.Default.Measure(triggerLabel);
+                        
+                        context.DrawString(
+                            triggerLabel,
+                            new Vector2(
+                                player.Rectangle.X - triggerMeasure.Width - 8,
+                                player.Rectangle.Y + (player.Rectangle.Height / 2f - triggerMeasure.Height / 2f)
+                            )
+                        );
+
+                        BlendTriggerBar(() =>
+                        {
+                            context.Rectangle(
+                                ShapeMode.Fill,
+                                player.Rectangle.X, 
+                                player.Rectangle.Y + player.Rectangle.Height / 2 - 8,
+                                -((triggerMeasure.Width + 16) * player.TriggerLeft),
+                                16,
+                                player.Color
+                            );
+                        });
+                    }
+
+                    if (player.TriggerRight > 0)
+                    {
+                        var triggerLabel = player.TriggerRight.ToString("F3");
+                        var triggerMeasure = TrueTypeFont.Default.Measure(triggerLabel);
+                        
+                        context.DrawString(
+                            triggerLabel,
+                            new Vector2(
+                                player.Rectangle.X + player.Rectangle.Width + 8,
+                                player.Rectangle.Y + (player.Rectangle.Height / 2f - triggerMeasure.Height / 2f)
+                            )
+                        );
+                        
+                        BlendTriggerBar(() =>
+                        {
+                            context.Rectangle(
+                                ShapeMode.Fill,
+                                player.Rectangle.X + player.Rectangle.Width, 
+                                player.Rectangle.Y + player.Rectangle.Height / 2 - 8,
+                                (triggerMeasure.Width + 16) * player.TriggerRight,
+                                16,
+                                player.Color
+                            );
+                        });
+                    }
+
+                    var typeLabel = controller.Info.Type.ToString();
+                    var typeMeasure = TrueTypeFont.Default.Measure(typeLabel);
+                    context.DrawString(
+                        typeLabel,
+                        new Vector2(
+                            player.Rectangle.X + (player.Rectangle.Width / 2f - typeMeasure.Width / 2f),
+                            player.Rectangle.Y + player.Rectangle.Height + 4
+                        ),
+                        player.Color
+                    );
+                    
+                    if (Math.Abs(player.RightStickX) > 0.09 || Math.Abs(player.RightStickY) > 0.09)
+                    {
+                        context.Circle(
+                            ShapeMode.Fill, 
+                            new Vector2(
+                                player.Rectangle.X + player.Rectangle.Width / 2 + (32 * player.RightStickX),
+                                player.Rectangle.Y + player.Rectangle.Height / 2  + (32 * player.RightStickY)
+                            ),
+                            6,
+                            RightStickHatColor
+                        );
+                    }
+                }
+            
+                context.DrawString(
+                    ViewName, 
+                    new(8, 8), 
+                    Color.White
+                );
+                
+                DrawViewSpecific(context);
+            });
+            
+            context.DrawTexture(
+                _renderTarget, 
+                PositionOnScreen
+            );
+        }
+
+        protected virtual void DrawViewSpecific(RenderContext context)
+        {
+        }
+        
+        private void BlendTriggerBar(Action renderLogic)
+        {
+            if (renderLogic == null)
+                return;
+            
+            RenderSettings.SetShapeBlendingEquations(
+                BlendingEquation.Subtract, 
+                BlendingEquation.Add
+            );
+            RenderSettings.SetShapeBlendingFunctions(
+                BlendingFunction.SourceColor,
+                BlendingFunction.One,
+                BlendingFunction.One,
+                BlendingFunction.Zero
+            );
+            
+            renderLogic();
+            
+            RenderSettings.ResetShapeBlending();
+        }
+    }
+}

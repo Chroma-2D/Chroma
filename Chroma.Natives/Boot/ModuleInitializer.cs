a4ce724d3e0869c56294442007c6c02757568888
Elijah Bansley <17408285+ElijahZAwesome@users.noreply.github.com>
2021-08-27 04:08:09 -0500

    #64: Fix MacOS build problems (#65)
    * MACOS MACOS MACOS MACOS
    
    Still need to fix freetype build
    
    * #64: Fixed MacOS freetype build and SDL_sound rpath
    
    * Revert freetype version

--- Chroma.Natives/Boot/ModuleInitializer.cs
+++ Chroma.Natives/Boot/ModuleInitializer.cs
@@ -106,7 +106,7 @@ namespace Chroma.Natives.Boot
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 Console.WriteLine("Warning! Your platform support is currently *untested*.\n" +
-                                  "Things can (and probably will) explode!");
+                                  "Things can (but probably won't) explode!");
                 
                 Platform = new MacPlatform();
             }

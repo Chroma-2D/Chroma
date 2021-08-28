1a9597f79e88eeef4c26e77e63ebcbe7e81de834
Ciastex <t.cichon.64@outlook.com>
2021-08-28 08:49:26 +0200

    remove extensibility for the time being

--- Chroma/Extensibility/AfterFixedUpdateAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Chroma.Extensibility
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class AfterFixedUpdateAttribute : Attribute
-    {
-    }
-}

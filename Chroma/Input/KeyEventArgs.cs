99b06735f534e7226820e4dd3dde4562e2b21510
vddCore <vddcore@outlook.com>
2021-09-11 07:07:45 +0200

    Add convenience properties for left/right modifiers keys

--- Chroma/Input/KeyEventArgs.cs
+++ Chroma/Input/KeyEventArgs.cs
@@ -6,6 +6,18 @@ namespace Chroma.Input
         public KeyCode KeyCode { get; }
         public KeyModifiers Modifiers { get; }
 
+        public bool IsAnyControlPressed
+            => AnyModifierOfTypePressed(KeyModifiers.LeftControl, KeyModifiers.RightControl);
+
+        public bool IsAnyAltPressed
+            => AnyModifierOfTypePressed(KeyModifiers.LeftAlt, KeyModifiers.RightAlt);
+
+        public bool IsAnySuperPressed
+            => AnyModifierOfTypePressed(KeyModifiers.LeftSuper, KeyModifiers.RightSuper);
+
+        public bool IsAnyShiftPressed
+            => AnyModifierOfTypePressed(KeyModifiers.LeftShift, KeyModifiers.RightShift);
+
         public bool IsRepeat { get; }
 
         internal KeyEventArgs(ScanCode scanCode, KeyCode keyCode, KeyModifiers modifiers, bool isRepeat)
@@ -15,6 +27,16 @@ namespace Chroma.Input
             Modifiers = modifiers;
 
             IsRepeat = isRepeat;
+        }
+
+        private bool AnyModifierOfTypePressed(params KeyModifiers[] keys)
+        {
+            var result = 0;
+
+            for (var i = 0; i < keys.Length; i++)
+                result |= (int)(Modifiers & keys[i]);
+
+            return result != 0;
         }
     }
 }
--- Chroma/Input/KeyModifiers.cs
+++ Chroma/Input/KeyModifiers.cs
@@ -8,19 +8,15 @@ namespace Chroma.Input
     {
         None = SDL2.SDL_Keymod.KMOD_NONE,
         
-        Shift = SDL2.SDL_Keymod.KMOD_SHIFT,
         LeftShift = SDL2.SDL_Keymod.KMOD_LSHIFT,
         RightShift = SDL2.SDL_Keymod.KMOD_RSHIFT,
         
-        Control = SDL2.SDL_Keymod.KMOD_CTRL,
         LeftControl = SDL2.SDL_Keymod.KMOD_LCTRL,
         RightControl = SDL2.SDL_Keymod.KMOD_RCTRL,
         
-        Alt = SDL2.SDL_Keymod.KMOD_ALT,
         LeftAlt = SDL2.SDL_Keymod.KMOD_LALT,
         RightAlt = SDL2.SDL_Keymod.KMOD_RALT,
         
-        Super = SDL2.SDL_Keymod.KMOD_GUI,
         LeftSuper = SDL2.SDL_Keymod.KMOD_LGUI,
         RightSuper = SDL2.SDL_Keymod.KMOD_RGUI,
         

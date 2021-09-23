9e6a1b4a6767c58ebdd29584933e4ffde7cd3360
Ciastex <t.cichon.64@outlook.com>
2021-09-23 18:37:58 +0200

    restore combined key modifiers

--- Chroma/Input/KeyModifiers.cs
+++ Chroma/Input/KeyModifiers.cs
@@ -10,15 +10,19 @@ namespace Chroma.Input
         
         LeftShift = SDL2.SDL_Keymod.KMOD_LSHIFT,
         RightShift = SDL2.SDL_Keymod.KMOD_RSHIFT,
+        Shift = (SDL2.SDL_Keymod.KMOD_LSHIFT | SDL2.SDL_Keymod.KMOD_RSHIFT),
         
         LeftControl = SDL2.SDL_Keymod.KMOD_LCTRL,
         RightControl = SDL2.SDL_Keymod.KMOD_RCTRL,
+        Control = (SDL2.SDL_Keymod.KMOD_LCTRL | SDL2.SDL_Keymod.KMOD_RCTRL),
         
         LeftAlt = SDL2.SDL_Keymod.KMOD_LALT,
         RightAlt = SDL2.SDL_Keymod.KMOD_RALT,
+        Alt = (SDL2.SDL_Keymod.KMOD_LALT | SDL2.SDL_Keymod.KMOD_RALT),
         
         LeftSuper = SDL2.SDL_Keymod.KMOD_LGUI,
         RightSuper = SDL2.SDL_Keymod.KMOD_RGUI,
+        Super = (SDL2.SDL_Keymod.KMOD_LGUI | SDL2.SDL_Keymod.KMOD_RGUI),
         
         NumLock = SDL2.SDL_Keymod.KMOD_NUM,
         CapsLock = SDL2.SDL_Keymod.KMOD_CAPS,

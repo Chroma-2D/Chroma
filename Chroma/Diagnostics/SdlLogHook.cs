7f26909eef24abe459ca82cb78b6606151b01b40
vddCore <vddcore@outlook.com>
2021-12-21 09:14:48 +0100

    Fix logger, move SDL log hook into diagnostics

--- Chroma/Diagnostics/SdlLogHook.cs
+++ Chroma/Diagnostics/SdlLogHook.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Runtime.InteropServices;
+using Chroma.Diagnostics.Logging;
 using Chroma.Natives.SDL;
 
-namespace Chroma.Diagnostics.Logging
+namespace Chroma.Diagnostics
 {
-    internal static class SdlLog
+    [LogName("SDL")]
+    internal static class SdlLogHook
     {
         private static readonly Log _log = LogManager.GetForCurrentAssembly();
         private static SDL2.SDL_LogOutputFunction _originalLogOutputFunction;
@@ -13,39 +15,38 @@ namespace Chroma.Diagnostics.Logging
         {
             if (_originalLogOutputFunction != null)
                 return;
-            
+
             SDL2.SDL_LogGetOutputFunction(out _originalLogOutputFunction, out _);
             SDL2.SDL_LogSetOutputFunction(LogOutputFunction, IntPtr.Zero);
         }
 
         private static void LogOutputFunction(
-            IntPtr _, 
-            int category, 
+            IntPtr _,
+            int category,
             SDL2.SDL_LogPriority priority,
             IntPtr message)
         {
             var messageString = Marshal.PtrToStringAuto(message);
-            
+
             switch (priority)
             {
                 case SDL2.SDL_LogPriority.SDL_LOG_PRIORITY_INFO:
                     _log.Info(messageString);
                     break;
-                
+
                 case SDL2.SDL_LogPriority.SDL_LOG_PRIORITY_WARN:
                     _log.Warning(messageString);
                     break;
-                
+
                 case SDL2.SDL_LogPriority.SDL_LOG_PRIORITY_ERROR:
                 case SDL2.SDL_LogPriority.SDL_LOG_PRIORITY_CRITICAL:
                     _log.Error(messageString);
                     break;
-                
+
                 case SDL2.SDL_LogPriority.SDL_LOG_PRIORITY_DEBUG:
                 case SDL2.SDL_LogPriority.SDL_LOG_PRIORITY_VERBOSE:
                     _log.Debug(messageString);
                     break;
-                
             }
         }
     }
